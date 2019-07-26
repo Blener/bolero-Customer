42057b0f9f2c69a52350c3f59049d881c2bdc8f4
ByBlener <blener@hotmail.com>
2019-07-26 16:04:55 -0300

    Initial commit with Customers GET from localhost api

---
+++ src/Customer.Bolero.Client/Main.fs
@@ -0,0 +1,274 @@
+module Customer.Bolero.Client.Main
+
+open System
+open Elmish
+open Bolero
+open Bolero.Html
+open Bolero.Json
+open Bolero.Remoting
+open Bolero.Remoting.Client
+
+/// Routing endpoints definition.
+type Page =
+    | [<EndPoint "/">] Home
+    | [<EndPoint "/counter">] Counter
+    | [<EndPoint "/data">] Data
+    | [<EndPoint "/customer">] Customer
+
+/// The Elmish application's model.
+type Model =
+    {
+        page: Page
+        counter: int
+        books: Book[] option
+        customers: Customer[] option
+        error: string option
+        username: string
+        password: string
+        signedInAs: option<string>
+        signInFailed: bool
+    }
+
+and Book =
+    {
+        title: string
+        author: string
+        [<DateTimeFormat "yyyy-MM-dd">]
+        publishDate: DateTime
+        isbn: string
+    }
+
+and Customer =
+    {
+        firstName: string
+        lastName: string
+    }
+
+let initModel =
+    {
+        page = Home
+        counter = 0
+        books = None
+        customers = None
+        error = None
+        username = ""
+        password = ""
+        signedInAs = None
+        signInFailed = false
+    }
+
+/// Remote service definition.
+type BookService =
+    {
+        /// Get the list of all books in the collection.
+        getBooks: unit -> Async<Book[]>
+
+        /// Add a book in the collection.
+        addBook: Book -> Async<unit>
+
+        /// Remove a book from the collection, identified by its ISBN.
+        removeBookByIsbn: string -> Async<unit>
+
+        /// Sign into the application.
+        signIn : string * string -> Async<option<string>>
+
+        /// Get the user's name, or None if they are not authenticated.
+        getUsername : unit -> Async<string>
+
+        /// Sign out from the application.
+        signOut : unit -> Async<unit>
+    }
+
+    interface IRemoteService with
+        member __.BasePath = "/books"
+
+type CustomerService =
+    {
+        getCustomers: unit -> Async<Customer[]>
+    }
+
+    interface IRemoteService with
+        member __.BasePath = "/customers"
+
+/// The Elmish application's update messages.
+type Message =
+    | SetPage of Page
+    | Increment
+    | Decrement
+    | SetCounter of int
+    | GetBooks
+    | GotBooks of Book[]
+    | SetUsername of string
+    | SetPassword of string
+    | GetSignedInAs
+    | RecvSignedInAs of RemoteResponse<string>
+    | SendSignIn
+    | RecvSignIn of option<string>
+    | SendSignOut
+    | RecvSignOut
+    | Error of exn
+    | ClearError
+
+    | GetCustomers
+    | GotCustomers of Customer[]
+
+let update bookRemote customerRemote message model =
+    let onSignIn = function
+        | Some _ -> Cmd.ofMsg GetBooks
+        | None -> Cmd.none
+    match message with
+    | SetPage page ->
+        { model with page = page }, Cmd.none
+
+    | Increment ->
+        { model with counter = model.counter + 1 }, Cmd.none
+    | Decrement ->
+        { model with counter = model.counter - 1 }, Cmd.none
+    | SetCounter value ->
+        { model with counter = value }, Cmd.none
+
+    | GetBooks ->
+        let cmd = Cmd.ofAsync bookRemote.getBooks () GotBooks Error
+        { model with books = None }, cmd
+    | GotBooks books ->
+        { model with books = Some books }, Cmd.none
+
+    | SetUsername s ->
+        { model with username = s }, Cmd.none
+    | SetPassword s ->
+        { model with password = s }, Cmd.none
+    | GetSignedInAs ->
+        model, Cmd.ofRemote bookRemote.getUsername () RecvSignedInAs Error
+    | RecvSignedInAs resp ->
+        let username = resp.TryGetResponse()
+        { model with signedInAs = username }, onSignIn username
+    | SendSignIn ->
+        model, Cmd.ofAsync bookRemote.signIn (model.username, model.password) RecvSignIn Error
+    | RecvSignIn username ->
+        { model with signedInAs = username; signInFailed = Option.isNone username }, onSignIn username
+    | SendSignOut ->
+        model, Cmd.ofAsync bookRemote.signOut () (fun () -> RecvSignOut) Error
+    | RecvSignOut ->
+        { model with signedInAs = None; signInFailed = false }, Cmd.none
+
+    | Error RemoteUnauthorizedException ->
+        { model with error = Some "You have been logged out."; signedInAs = None }, Cmd.none
+    | Error exn ->
+        { model with error = Some exn.Message }, Cmd.none
+    | ClearError ->
+        { model with error = None }, Cmd.none
+
+    | GetCustomers ->
+        let cmd = Cmd.ofAsync customerRemote.getCustomers () GotCustomers Error
+        { model with error = None }, cmd
+    | GotCustomers customers ->
+        { model with customers = Some customers }, Cmd.none
+
+/// Connects the routing system to the Elmish application.
+let router = Router.infer SetPage (fun model -> model.page)
+
+type Main = Template<"wwwroot/main.html">
+
+let homePage model dispatch =
+    Main.Home().Elt()
+
+let counterPage model dispatch =
+    Main.Counter()
+        .Decrement(fun _ -> dispatch Decrement)
+        .Increment(fun _ -> dispatch Increment)
+        .Value(model.counter, fun v -> dispatch (SetCounter v))
+        .Elt()
+
+let dataPage model (username: string) dispatch =
+    Main.Data()
+        .Reload(fun _ -> dispatch GetBooks)
+        .Username(username)
+        .SignOut(fun _ -> dispatch SendSignOut)
+        .Rows(cond model.books <| function
+            | None ->
+                Main.EmptyData().Elt()
+            | Some books ->
+                forEach books <| fun book ->
+                    tr [] [
+                        td [] [text book.title]
+                        td [] [text book.author]
+                        td [] [text (book.publishDate.ToString("yyyy-MM-dd"))]
+                        td [] [text book.isbn]
+                    ])
+        .Elt()
+
+let customersPage model dispatch =
+    Main.Customers()
+        .Reload(fun _ -> dispatch GetCustomers)
+        .Rows(cond model.customers <| function
+            | None ->
+                Main.NoCustomers().Elt()
+            | Some customers ->
+                forEach customers <| fun customer ->
+                    tr [] [
+                        td [] [text customer.firstName]
+                        td [] [text customer.lastName]
+                    ])
+        .Elt()
+
+let signInPage model dispatch =
+    Main.SignIn()
+        .Username(model.username, fun s -> dispatch (SetUsername s))
+        .Password(model.password, fun s -> dispatch (SetPassword s))
+        .SignIn(fun _ -> dispatch SendSignIn)
+        .ErrorNotification(
+            cond model.signInFailed <| function
+            | false -> empty
+            | true ->
+                Main.ErrorNotification()
+                    .HideClass("is-hidden")
+                    .Text("Sign in failed. Use any username and the password \"password\".")
+                    .Elt()
+        )
+        .Elt()
+
+let menuItem (model: Model) (page: Page) (text: string) =
+    Main.MenuItem()
+        .Active(if model.page = page then "is-active" else "")
+        .Url(router.Link page)
+        .Text(text)
+        .Elt()
+
+let view model dispatch =
+    Main()
+        .Menu(concat [
+            menuItem model Home "Home"
+            menuItem model Counter "Counter"
+            menuItem model Data "Download data"
+            menuItem model Customer "Customer"
+        ])
+        .Body(
+            cond model.page <| function
+            | Home -> homePage model dispatch
+            | Counter -> counterPage model dispatch
+            | Data ->
+                cond model.signedInAs <| function
+                | Some username -> dataPage model username dispatch
+                | None -> signInPage model dispatch
+            | Customer -> customersPage model dispatch
+        )
+        .Error(
+            cond model.error <| function
+            | None -> empty
+            | Some err ->
+                Main.ErrorNotification()
+                    .Text(err)
+                    .Hide(fun _ -> dispatch ClearError)
+                    .Elt()
+        )
+        .Elt()
+
+type MyApp() =
+    inherit ProgramComponent<Model, Message>()
+
+    override this.Program =
+        let bookService = this.Remote<BookService>()
+        let customerService = this.Remote<CustomerService>()
+        let update = update bookService customerService
+        Program.mkProgram (fun _ -> initModel, Cmd.ofMsg GetSignedInAs) update view
+        |> Program.withRouter router
---
+++ src/Customer.Bolero.Server/Startup.fs
@@ -0,0 +1,104 @@
+namespace Customer.Bolero.Server
+
+open System
+open System.IO
+open System.Net.Http
+open Microsoft.AspNetCore
+open Microsoft.AspNetCore.Authentication.Cookies
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Bolero
+open Bolero.Remoting
+open Bolero.Remoting.Server
+open Customer.Bolero
+
+type BookService(env: IWebHostEnvironment) =
+    inherit RemoteHandler<Client.Main.BookService>()
+
+    let books =
+        Path.Combine(env.ContentRootPath, "data/books.json")
+        |> File.ReadAllText
+        |> Json.Deserialize<Client.Main.Book[]>
+        |> ResizeArray
+
+    override this.Handler =
+        {
+            getBooks = Remote.authorize <| fun _ () -> async {
+                return books.ToArray()
+            }
+
+            addBook = Remote.authorize <| fun _ book -> async {
+                books.Add(book)
+            }
+
+            removeBookByIsbn = Remote.authorize <| fun _ isbn -> async {
+                books.RemoveAll(fun b -> b.isbn = isbn) |> ignore
+            }
+
+            signIn = Remote.withContext <| fun http (username, password) -> async {
+                if password = "password" then
+                    do! http.AsyncSignIn(username, TimeSpan.FromDays(365.))
+                    return Some username
+                else
+                    return None
+            }
+
+            signOut = Remote.withContext <| fun http () -> async {
+                return! http.AsyncSignOut()
+            }
+
+            getUsername = Remote.authorize <| fun http () -> async {
+                return http.User.Identity.Name
+            }
+        }
+
+type CustomerService() =
+    inherit RemoteHandler<Client.Main.CustomerService>()
+
+    override __.Handler =
+        {
+            getCustomers = Remote.withContext <| fun _ () -> async {
+                let client = new HttpClient()
+                let! customersJson = client.GetStringAsync("http://localhost:57285") |> Async.AwaitTask
+                return Json.Deserialize<Client.Main.Customer[]> customersJson
+            }
+        }
+
+type Startup() =
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    member this.ConfigureServices(services: IServiceCollection) =
+        services.AddMvcCore() |> ignore
+        services
+            .AddAuthorization()
+            .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie()
+                .Services
+            .AddRemoting<BookService>()
+            .AddRemoting<CustomerService>()
+        |> ignore
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    member this.Configure(app: IApplicationBuilder, env: IWebHostEnvironment) =
+        app
+            .UseAuthentication()
+            .UseRemoting()
+            .UseClientSideBlazorFiles<Client.Startup>()
+            .UseRouting()
+            .UseEndpoints(fun endpoints ->
+                endpoints.MapDefaultControllerRoute() |> ignore
+                endpoints.MapFallbackToClientSideBlazor<Client.Startup>("index.html") |> ignore)
+        |> ignore
+
+module Program =
+
+    [<EntryPoint>]
+    let main args =
+        WebHost
+            .CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .Build()
+            .Run()
+        0
